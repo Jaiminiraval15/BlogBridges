4f74821b165f46bd7d1edee7adfd0193f8f450df
Jaimini <jaiminiraval123@gmail.com>
2024-01-23 19:37:40 +0530

    Add useLogin hook

---
+++ client/src/hooks/useLogin.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+import { useNavigate } from 'react-router-dom'; 
+export const useLogin = () => {
+    const [error,setError] = useState(null)
+    const [isLoading,setIsLoading] = useState(false)
+    const { dispatch } = useAuthContext()
+    const { dispatch: encryptionDispatch } = useEncryptionContext();
+    const { generateKey } = useEncryptionFunction()
+    const navigate = useNavigate();
+    const login = async (email,password) =>{
+        setIsLoading(true)
+        setError(null)
+        const res = await fetch('http://localhost:2000/api/routes/login',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email,password})
+        })
+        const data = await res.json()
+        if (!res.ok){
+            setIsLoading(false)
+            setError(data.error)
+        }
+        if(res.ok){
+   
+            //save the user to session
+            sessionStorage.setItem('user',JSON.stringify(data))
+            //update authContext
+            dispatch({type: 'LOGIN',payload:data})
+            setIsLoading(false)
+         
+
+        }
+
+    }
+    return {isLoading,error,login}
+}
