322c98da0a8f39eeba1175083691c1fc4c4a66d2
Jaimini <jaiminiraval123@gmail.com>
2024-01-23 19:41:46 +0530

    Add useSignup and useLogout hook

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -1,20 +1,19 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 
-import { useNavigate } from 'react-router-dom'; 
-export const useLogin = () => {
+export const useSignup = () => {
     const [error,setError] = useState(null)
     const [isLoading,setIsLoading] = useState(false)
-    const { dispatch } = useAuthContext()
-  
-    const navigate = useNavigate();
-    const login = async (email,password) =>{
+    const { dispatch } = useAuthContext();
+   
+    
+    const signup = async (email,password,firstname,lastname,username) =>{
         setIsLoading(true)
         setError(null)
-        const res = await fetch('http://localhost:2000/api/routes/login',{
+        const res = await fetch('http://localhost:2000/api/routes/signup',{
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({email,password})
+        body: JSON.stringify({email,password,firstname,lastname,username})
         })
         const data = await res.json()
         if (!res.ok){
@@ -22,16 +21,13 @@ export const useLogin = () => {
             setError(data.error)
         }
         if(res.ok){
-   
             //save the user to session
             sessionStorage.setItem('user',JSON.stringify(data))
             //update authContext
             dispatch({type: 'LOGIN',payload:data})
             setIsLoading(false)
-         
-
         }
 
     }
-    return {isLoading,error,login}
+    return {isLoading,error,signup}
 }
